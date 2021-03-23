5ac1ebe3cf8dad633b8d189b2bf2328417813d23
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-03-23 22:56:29 +0000

    login update
    added SQL injection attack protection

--- logIn.php
+++ logIn.php
@@ -19,23 +19,27 @@
                 if (empty($email)) { array_push($errors, "Email is required"); }
                 if (empty($password)) { array_push($errors, "Password is required");  }
             
-                $query = "SELECT * FROM users WHERE Email='$email'";
-                $result = mysqli_query($connection, $query);
-                $user = mysqli_fetch_assoc($result);
+                $emailCheck = $connection->prepare('SELECT * FROM users WHERE Email = ?');
+                $emailCheck->bind_param('s', $email);
+                $emailCheck->execute();
 
+                $result = $emailCheck->get_result();
+                $user = mysqli_fetch_assoc($result);
+    
                 if( mysqli_num_rows($result) == 1) {
                     if(!password_verify($password, $user['Password'])) {  array_push($errors, "Password is incorrect"); }
                 } else {
-                        array_push($errors, "No account with given email");
+                    array_push($errors, "No account with given email");
                 }
 
                 if (count($errors) == 0) {
                         $_SESSION['userID'] = $user['UserID'];
                         $_SESSION['success'] = "You are now logged in";
-                        header("location: Home.php");
                         echo "success";
+                        header("location: Home.php");
                 } else {
                     echo $errors[0];
+                    header("location: index.html");
                 }
             }
             
