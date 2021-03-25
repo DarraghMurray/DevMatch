0fdd03fd47ca54831a9fc840ad7d79071e6a58e7
Cristina P <32393477+crisapal@users.noreply.github.com>
2021-03-25 19:43:20 +0000

    Add files via upload

---
+++ fin/myConnections.php
@@ -0,0 +1,79 @@
+<html>
+    <head>
+      <link rel = "stylesheet"
+			href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+			integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+			crossorigin="anonymous">
+    </head>
+
+    <body>
+      <?php
+        include("database.php");
+		session_start();
+        $user= $_SESSION['userID'];
+	
+        $searchTerm =  $user ;
+        $connectionSearch = $connection->prepare('SELECT User2ID FROM connections WHERE User1ID = ?');
+        $connectionSearch->bind_param('s',$searchTerm);
+        $connectionSearch->execute();
+
+
+        $result = $connectionSearch->get_result();
+
+		displaySearchResultProfile($result);
+    
+
+		
+	// Display search result for the different search
+	// Not ideal, relevant field are hard coded and each search has a different display function.
+	function displaySearchResultProfile($mysqlResult){	 
+		echo ('
+		<div class="container">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h2 class="panel-title">My Connections</h2>
+				</div>
+			<table class="table table-bordered table-condensed table-hover">
+			<thead class="thead-dark">
+				<tr>');
+			/*while($finfo=$mysqlResult->fetch_field()){ //We don't want every field
+				echo ('
+					<th>'.$finfo->name.'</th>
+				');
+			}*/
+				echo('
+					<th>Name</th>
+					<th>Gender</th>
+					<th>Country</th>
+					<th>Employment</th>');
+			echo ('
+				</tr>
+			</thead>');
+			while($row = mysqli_fetch_assoc($mysqlResult)) {
+				echo('<tr class="clickableRow" data-href="#">
+					<td>
+						'.$row['FirstName'].' '.$row['LastName'].'
+					</td>
+					<td>
+						'.$row['Gender'].'
+					</td>
+					<td>
+						'.$row['Country'].'
+					</td>
+					<td>
+						'.$row['Employment'].'
+					</td>
+				</tr>');
+			}
+		echo ('
+		</table>
+		</div>
+		</div>');
+	}
+	
+
+	
+    ?>
+    </body>
+</html>
+
