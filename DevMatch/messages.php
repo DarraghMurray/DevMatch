80e0c43de2c844a38713ec92acdb583a43a32f25
alfredpm <alfredpermez@gmail.com>
2021-05-02 14:26:15 +0100

    Merged Cris message animation

--- DevMatch/messages.php
+++ DevMatch/messages.php
@@ -1,6 +1,6 @@
 <?php
         require("navBar.php");
-        require("database.php");
+/*         require("database.php");
 		
         $user= $_SESSION['userID'];
 	
@@ -11,19 +11,31 @@
 
         $result = $connectionSearch->get_result();
         $row = mysqli_fetch_assoc($result);
-        echo $row['FirstName'].' '.$row['LastName'];
+        echo $row['FirstName'].' '.$row['LastName']; */
 
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-      <link rel = "stylesheet"
+ 		<!--<link rel = "stylesheet"
 			href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
 			integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
-			crossorigin="anonymous">
+			crossorigin="anonymous"> -->
+		<link href="https://fonts.googleapis.com/css2?family=Khula:wght@700&display=swap" rel="stylesheet">
 
-    </head>
-    <body>
+		<link rel="stylesheet" href="CSS/animation.css">
+	</head>
+	<body>
+		<div class="loader">
+			<div class="loader-item loader-item_1"></div>
+			<div class="loader-item loader-item_2"></div>
+			<div class="loader-item loader-item_3"></div>
+			<div class="loader-item loader-item_4"></div>
+		</div>
+		<div class='console-container'><span id='text'></span><div class='console-underscore' id='console'>&#95;</div></div>
+
+		<script type="text/javascript" src="animation.js">
+		</script>
     </body>
 </html>
