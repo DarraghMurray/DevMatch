1ea622889b67a3cd43778bbe353280ceaef6e8f6
Cristina P <32393477+crisapal@users.noreply.github.com>
2021-05-05 07:35:49 +0100

    replace deleted

--- DevMatchFinal/DevMatch/delete-teams.php
+++ DevMatchFinal/DevMatch/delete-teams.php
@@ -1,41 +1,79 @@
 <?php
-        require("navBar.php");
-/*         require("database.php");
-		
-        $user= $_SESSION['userID'];
-	
-        $searchTerm =  $user ;
-        $connectionSearch = $connection->prepare('SELECT * FROM connections WHERE UserID = ?');
-        $connectionSearch->bind_param('s',$searchTerm);
-        $connectionSearch->execute();
-
-        $result = $connectionSearch->get_result();
-        $row = mysqli_fetch_assoc($result);
-        echo $row['FirstName'].' '.$row['LastName']; */
-
+  require("navBar.php");
 ?>
 
-<!DOCTYPE html>
 <html>
     <head>
- 		<link rel = "stylesheet"
+      <link rel = "stylesheet"
 			href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
 			integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
 			crossorigin="anonymous">
-		<link href="https://fonts.googleapis.com/css2?family=Khula:wght@700&display=swap" rel="stylesheet">
+      <link href="http://netdna.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="CSS/profile.css">
 
-		<link rel="stylesheet" href="CSS/animation.css">
-	</head>
-	<body>
-		<div class="loader">
-			<div class="loader-item loader-item_1"></div>
-			<div class="loader-item loader-item_2"></div>
-			<div class="loader-item loader-item_3"></div>
-			<div class="loader-item loader-item_4"></div>
-		</div>
-		<div class='console-container'><span id='text'></span><div class='console-underscore' id='console'>&#95;</div></div>
+    </head>
+    <body>
 
-		<script type="text/javascript" src="animation.js">
-		</script>
+
+<?php
+        require("database.php");
+        $user = $_SESSION['userID'];
+
+
+		echo ('
+		<div class="container">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h2 class="panel-title">My teams delete</h2>
+				</div>
+			<table class="table table-bordered table-condensed table-hover">
+			<thead class="thead-dark">
+				<tr>');
+
+					echo('
+					<th>Name</th>
+					<th>Description</th>
+				');
+			echo ('
+				</tr>
+			</thead>');
+
+
+		$params=array($user);
+		$getTeamID = $db->executeStatement('SELECT * FROM teams WHERE CreatorID= ?','s',$params);
+       
+        $result = $getTeamID->get_result();
+
+        while($row = mysqli_fetch_assoc($result)){
+			echo ('<tr class="clickableRow" data-href="#">
+			<td>
+				'.$row['Name'].'
+			</td>
+			<td>
+				'.$row['Description'].'
+			</td>
+			<td>
+			<form action="" method="post">
+				<input type="hidden" name="del"'.$row['TeamID'].' value=' .$row['TeamID']. '>
+				<input type="submit" name="Delete" value="Delete" />
+');
+			if(isset($_POST["Delete"])){
+			$params = array($row['TeamID']);
+			$del = $db->executeStatement('DELETE FROM teams WHERE TeamID= ?','i',$params);
+			$del = $db->executeStatement('DELETE FROM members WHERE TeamID = ?','i',$params);
+			}
+			echo ('</form> </td>');
+		}
+
+		echo (' </table>
+				</div>
+				</div>');
+	
+
+	?>
+
+  
+      
     </body>
 </html>
+
